e1cc8d7f6039dddc737684b8837be0dcf82ce85c
Sergiotg03 <sergiotg003@gmail.com>
2025-02-24 20:54:43 +0100

    style: change app icon and name
    issue: #47

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,8 @@
   <nav>
     <RouterLink to="/">Go to Home</RouterLink>
     <RouterLink to="/pets">Go to Pets</RouterLink>
+    <RouterLink to="/componentesSergio">Go to componentes Sergio</RouterLink>
+    <RouterLink to="/componentesLuis">Go to componentes Luis</RouterLink>
   </nav>
   <main>
     <RouterView />
