7b01305c26be3bc0fe94128ec5b28ee7da39918e
LuisMelladoDiaz <melladodiazluis@gmail.com>
2025-02-25 18:51:55 +0100

    style(theme): add colors, fonts and logos
    issue: #47

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,4 @@
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
-  </p>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/pets">Go to Pets</RouterLink>
-    <RouterLink to="/componentesSergio">Go to componentes Sergio</RouterLink>
-    <RouterLink to="/componentesLuis">Go to componentes Luis</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
