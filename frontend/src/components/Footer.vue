f1cff4d1d4ee1ca3e61e3fefff07d36c32b3f70c
LuisMelladoDiaz <melladodiazluis@gmail.com>
2025-02-25 18:55:18 +0100

    feat(components): create footer
    issue: #47

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer">
+    <p>© 2025 Pawtel (Grupo 11). Todos los derechos reservados.</p>
+  </footer>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.footer {
+  background-color: #C36C6C; 
+  color: white;
+  text-align: center;
+  padding: 15px;
+  font-size: 16px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+}
+
+.footer p {
+  margin: 0;
+}
+</style>
