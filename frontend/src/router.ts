5cc1cfefd9ea7df3ec857438b839d8fe812ff110
Sergio TG <sertregon@alum.us.es>
2025-02-26 19:27:37 +0100

    feat(components): add template screens
    #47

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -4,12 +4,16 @@ import HomeView from './views/HomeView.vue'
 import PetsView from './views/PetsView.vue'
 import ComponentesLuis from './views/ComponentesLuis.vue'
 import ComponentesSergio from './views/ComponentesSergio.vue'
+import TemplateScreen1 from './views/TemplateScreen1.vue'
+import TemplateScreen2 from './views/TemplateScreen2.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/pets', component: PetsView},
   { path: '/componentesLuis', component: ComponentesLuis},
-  { path: '/componentesSergio', component: ComponentesSergio}
+  { path: '/componentesSergio', component: ComponentesSergio},
+  { path: '/templateScreen1', component: TemplateScreen1},
+  { path: '/templateScreen2', component: TemplateScreen2}
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/TemplateScreen2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NavbarTerracota />
+
+    <div class="max-w-7xl mx-auto px-5">
+
+      Aquí va el contenido
+
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Navbar from '../components/Navbar.vue';
+import NavbarTerracota from '../components/NavbarTerracota.vue';
+import FilterNavbar from '../components/FilterNavbar.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+<style scoped>
+/* Estilos adicionales */
+</style>
