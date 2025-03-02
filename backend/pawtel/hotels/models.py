77623432bc10ed7885a086d0348c8ef98d1b2b39
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2025-03-02 23:55:07 +0100

    style(models): improve readability of models
    It will anyways be changed by the pre-commit more than I would like.

--- backend/pawtel/hotels/models.py
+++ backend/pawtel/hotels/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-from pawtel.hotelOwners.models import HotelOwner
+from pawtel.hotel_owners.models import HotelOwner
 
 
 class Hotel(models.Model):
 
+    # Attributes -------------------------------------------------------------
+
     is_archived = models.BooleanField(default=False, null=False)
-    hotel_owner = models.ForeignKey(HotelOwner, on_delete=models.CASCADE, null=False)
+
     name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+
     address = models.CharField(max_length=100, blank=False, null=False)
+
     city = models.CharField(max_length=50, blank=False, null=False)
+
     description = models.TextField(max_length=300, blank=False, null=False)
+
+    # Relations --------------------------------------------------------------
+
+    hotel_owner = models.ForeignKey(HotelOwner, on_delete=models.CASCADE, null=False)
+
+    # Meta configuration -----------------------------------------------------
 
     def __str__(self):
         return f"Hotel {self.name}, {self.city}"
--- backend/pawtel/rooms/models.py
+++ backend/pawtel/rooms/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-from pawtel.roomTypes.models import RoomType
+from pawtel.room_types.models import RoomType
 
 
 class Room(models.Model):
 
+    # Attributes -------------------------------------------------------------
+
     is_archived = models.BooleanField(default=False, null=False)
-    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, null=False)
+
     name = models.CharField(max_length=50, blank=False, null=False)
+
+    # Relations --------------------------------------------------------------
+
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE, null=False)
+
+    # Meta configuration -----------------------------------------------------
 
     def __str__(self):
         return f"{self.name} ({self.room_type.name})"
