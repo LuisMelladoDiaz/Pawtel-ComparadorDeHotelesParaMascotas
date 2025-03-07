33a266f174c99277d1691b14e30921b566b00ef6
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2025-03-07 16:22:46 +0100

    test(hotel-owner): add test for service. #68

---
+++ backend/pawtel/hotels/tests/test_services.py
@@ -0,0 +1,91 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from pawtel.hotel_owners.models import HotelOwner
+from pawtel.hotels.models import Hotel
+from pawtel.hotels.services import HotelService
+from pawtel.room_types.models import RoomType
+from pawtel.rooms.models import Room
+from rest_framework.exceptions import NotFound, PermissionDenied
+
+
+class HotelServiceTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testowner", password="1234")
+        self.hotel_owner = HotelOwner.objects.create(
+            username=self.user.username,
+            email="owner@example.com",
+            phone="+34123456789",
+            is_active=True,
+        )
+        self.hotel = Hotel.objects.create(
+            name="Test Hotel", is_archived=False, hotel_owner=self.hotel_owner
+        )
+        self.archived_hotel = Hotel.objects.create(
+            name="Archived Hotel", is_archived=True, hotel_owner=self.hotel_owner
+        )
+
+        self.room_type1 = RoomType.objects.create(
+            name="Single",
+            hotel=self.hotel,
+            description="A cozy single room.",
+            capacity=1,
+            price_per_night=50.0,
+            pet_type="DOG",
+        )
+
+        self.room_type2 = RoomType.objects.create(
+            name="Double",
+            hotel=self.hotel,
+            description="A spacious double room.",
+            capacity=2,
+            price_per_night=75.0,
+            pet_type="CAT",
+        )
+
+        Room.objects.create(room_type=self.room_type1)
+        Room.objects.create(room_type=self.room_type2)
+        Room.objects.create(room_type=self.room_type2)
+
+    def test_get_all_room_types_of_hotel(self):
+        room_types = HotelService.get_all_room_types_of_hotel(self.hotel.id)
+        self.assertEqual(len(room_types), 2)
+        self.assertEqual(room_types[0]["name"], "Single")
+        self.assertEqual(room_types[1]["name"], "Double")
+
+    def test_get_all_room_types_of_hotel_not_found(self):
+        with self.assertRaises(NotFound):
+            HotelService.get_all_room_types_of_hotel(999)
+
+    def test_get_total_vacancy_for_each_room_type_of_hotel(self):
+        vacancies = HotelService.get_total_vacancy_for_each_room_type_of_hotel(
+            self.hotel.id
+        )
+        self.assertEqual(len(vacancies), 2)
+
+        single_vacancy = next(
+            v for v in vacancies if v["room_type_id"] == self.room_type1.id
+        )
+        double_vacancy = next(
+            v for v in vacancies if v["room_type_id"] == self.room_type2.id
+        )
+
+        self.assertEqual(single_vacancy["total_vacancy"], 1)
+        self.assertEqual(double_vacancy["total_vacancy"], 4)
+
+    def test_get_total_vacancy_for_each_room_type_of_hotel_not_found(self):
+        with self.assertRaises(NotFound):
+            HotelService.get_total_vacancy_for_each_room_type_of_hotel(999)
+
+    def test_check_if_archived(self):
+        try:
+            HotelService.check_if_archived(self.hotel.id)
+        except PermissionDenied:
+            self.fail(
+                "check_if_archived raised an unexpected exception for an active hotel."
+            )
+        with self.assertRaises(PermissionDenied):
+            HotelService.check_if_archived(self.archived_hotel.id)
+
+    def test_check_if_archived_not_found(self):
+        with self.assertRaises(NotFound):
+            HotelService.check_if_archived(999)
