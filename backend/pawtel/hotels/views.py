470d1b31bb0d9446334d74211872a8c1bb7f6869
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2025-03-09 02:18:04 +0100

    fix(hotel): fix controller #68

--- backend/pawtel/hotels/views.py
+++ backend/pawtel/hotels/views.py
@@ -1,7 +1,8 @@
 from pawtel.hotels.models import Hotel
 from pawtel.hotels.serializers import HotelSerializer
 from pawtel.hotels.services import HotelService
-from rest_framework import viewsets
+from pawtel.room_types.serializers import RoomTypeSerializer
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -10,31 +11,66 @@ class HotelViewSet(viewsets.ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
 
-    @action(detail=True, methods=["get"])
-    def get_all_room_types_of_hotel(self, request, pk=None):
-        hotel_id = pk
-        room_types = HotelService.get_all_room_types_of_hotel(hotel_id)
-        return Response(room_types)
+    def list(self, request):
+        hotels = Hotel.objects.filter(is_archived=False)
+        serializer = HotelSerializer(hotels, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=["get"])
-    def get_total_vacancy_for_each_room_type_of_hotel(self, request, pk=None):
-        hotel_id = pk
-        vacancy_data = HotelService.get_total_vacancy_for_each_room_type_of_hotel(
-            hotel_id
+    def retrieve(self, request, pk=None):
+        HotelService.authorize_action_hotel(request, pk)
+        hotel = HotelService.retrieve_hotel(pk)
+        serializer = HotelSerializer(hotel)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        input_serializer = HotelService.serialize_input_hotel_create(request)
+        HotelService.validate_create_hotel(input_serializer)
+        hotel_created = HotelService.create_hotel(input_serializer)
+        output_serializer_data = HotelService.serialize_output_hotel(hotel_created)
+        return Response(output_serializer_data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        HotelService.authorize_action_hotel(request, pk)
+        input_serializer = HotelService.serialize_input_hotel_update(request, pk)
+        HotelService.validate_update_hotel(pk, input_serializer)
+        hotel_updated = HotelService.update_hotel(pk, input_serializer)
+        output_serializer_data = HotelService.serialize_output_hotel(hotel_updated)
+        return Response(output_serializer_data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk=None):
+        HotelService.authorize_action_hotel(request, pk)
+        input_serializer = HotelService.serialize_input_hotel_partial_update(
+            request, pk
         )
-        return Response(vacancy_data)
+        HotelService.validate_partial_update_hotel(pk, input_serializer)
+        hotel_updated = HotelService.partial_update_hotel(pk, input_serializer)
+        output_serializer_data = HotelService.serialize_output_hotel(hotel_updated)
+        return Response(output_serializer_data, status=status.HTTP_200_OK)
 
-    def update(self, request, *args, **kwargs):
-        hotel_owner = self.get_object()
-        HotelService.check_if_archived(hotel_owner.id)
-        return super().update(request, *args, **kwargs)
+    def destroy(self, request, pk=None):
+        HotelService.authorize_action_hotel(request, pk)
+        HotelService.delete_hotel(pk)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def partial_update(self, request, *args, **kwargs):
-        hotel_owner = self.get_object()
-        HotelService.check_if_archived(hotel_owner.id)
-        return super().partial_update(request, *args, **kwargs)
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path="room-types",
+        url_name="get_all_room_types_of_hotel",
+    )
+    def get_all_room_types_of_hotel(self, request, pk=None):
+        room_types = HotelService.get_all_room_types_of_hotel(request, pk)
+        serializer = RoomTypeSerializer(room_types, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def destroy(self, request, *args, **kwargs):
-        hotel_owner = self.get_object()
-        HotelService.check_if_archived(hotel_owner.id)
-        return super().destroy(request, *args, **kwargs)
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path="room-types/total-vacancy",
+        url_name="get_total_vacancy_for_each_room_type_of_hotel",
+    )
+    def get_total_vacancy_for_each_room_type_of_hotel(self, request, pk=None):
+        vacancy_data = HotelService.get_total_vacancy_for_each_room_type_of_hotel(
+            request, pk
+        )
+        return Response(vacancy_data, status=status.HTTP_200_OK)
