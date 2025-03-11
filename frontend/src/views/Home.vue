b9cb8ab8494d2821cd18a9b23da195cb02974d41
David González Martínez <me@davidgonmar.me>
2025-03-11 19:10:37 +0100

    fix: bad import

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Navbar from '../components/Navbar.vue';
+import Navbar from '../components/NavBar.vue';
 import Footer from '../components/Footer.vue';
 import FilterNavbar from '../components/FilterNavbar.vue';
 import Carrusel from '../components/Carrusel.vue';
