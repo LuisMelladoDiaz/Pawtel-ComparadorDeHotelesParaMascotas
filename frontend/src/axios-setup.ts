2b63f1db1ea522d8d12e4c68fb4afccd8b5f4f26
David González Martínez <me@davidgonmar.me>
2025-03-12 14:25:55 +0100

    fix: auth thing

--- frontend/src/axios-setup.ts
+++ frontend/src/axios-setup.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export function refreshAxiosInterceptor() {
   axios.interceptors.request.use((config) => {
     const token = localStorage.getItem("access_token");
-    console.log(config)
     // Check if the request URL starts with "/auth"
     if (token && config.url && !config.url.includes("/auth")) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -15,3 +14,17 @@ export function refreshAxiosInterceptor() {
     return config;
   });
 }
+
+// if 401 is received, delete the access token and refresh token
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      refreshAxiosInterceptor();
+    }
+
+    return Promise.reject(error);
+  }
+);
