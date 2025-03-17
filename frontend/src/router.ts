707411f834f6718d1bdb00604ce096f600b8250a
LuisMelladoDiaz <melladodiazluis@gmail.com>
2025-03-17 12:58:29 +0100

    refactor(views): rename hotelOwnerPanel to mis-hoteles

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HotelDetailsView from './views/HotelDetailView.vue';
-import DevelopmentScreen from './views/DevelopmentScreen.vue'
-import Componentes1 from './views/Componentes1.vue'
-import Componentes2 from './views/Componentes2.vue'
-import TemplateScreen1 from './views/TemplateScreen1.vue'
-import TemplateScreen2 from './views/TemplateScreen2.vue'
-import UserProfile from './views/UserProfile.vue'
-import HotelListScreen from './views/HotelListScreen.vue'
-import LoginView from './views/LoginView.vue'
-import RegisterView from './views/RegisterView.vue'
-import HotelOwnerPanel from './views/HotelOwnerPanel.vue'
-import Home from './views/Home.vue'
 import AboutUs from './views/AboutUs.vue';
+import Componentes1 from './views/Componentes1.vue';
+import Componentes2 from './views/Componentes2.vue';
 import Contact from './views/Contact.vue';
+import DevelopmentScreen from './views/DevelopmentScreen.vue';
+import Home from './views/Home.vue';
+import HotelDetailsView from './views/HotelDetailView.vue';
+import HotelListScreen from './views/HotelListScreen.vue';
+import HotelOwnerPanel from './views/HotelOwnerPanel.vue';
+import LoginView from './views/LoginView.vue';
+import MisHoteles from './views/MisHoteles.vue';
+import RegisterView from './views/RegisterView.vue';
+import TemplateScreen1 from './views/TemplateScreen1.vue';
+import TemplateScreen2 from './views/TemplateScreen2.vue';
+import UserProfile from './views/UserProfile.vue';
 
 
 const routes = [
@@ -31,6 +32,7 @@ const routes = [
   { path: '/template-screen-2', component: TemplateScreen2},
   { path: '/user-profile', component: UserProfile},
   { path: '/hotel-owner-panel', component: HotelOwnerPanel },
+  { path: '/mis-hoteles', component: MisHoteles },
 ]
 
 const router = createRouter({
