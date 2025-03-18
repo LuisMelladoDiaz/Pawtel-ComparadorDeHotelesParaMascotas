76207195ea3904ae8010637e7465c29f138d5ab4
KSTY <mancasvel@icloud.com>
2025-03-18 20:44:33 +0100

    fix(forms): register and login form with vee validators fixed with custom errors #158

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,130 +3,74 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import NavbarTerracota from '../components/NavBarTerracota.vue';
 import Footer from '../components/Footer.vue';
-import { useLoginMutation, useLogoutMutation } from '@/data-layer/auth';
+import { useLoginMutation } from '@/data-layer/auth';
 import { Notyf } from 'notyf';
-import { useField, Form, Field, ErrorMessage } from 'vee-validate';
-import { required } from '@vee-validate/rules';
-import { defineRule } from 'vee-validate';
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
 
 const notyf = new Notyf();
-
-defineRule('required', required);
-
-const username = ref('');
-const password = ref('');
-const showPassword = ref(false);
-const errorMessage = ref('');
 const router = useRouter();
 const loginMutation = useLoginMutation();
+const showPassword = ref(false);
+const errorMessage = ref('');
 
-const togglePasswordVisibility = () => {
-    showPassword.value = !showPassword.value;
+// Esquema de validación con Yup
+const validationSchema = yup.object({
+    username: yup.string().required('El nombre de usuario es obligatorio'),
+    password: yup.string().required('La contraseña es obligatoria')
+});
+
+
+
+const login = (values) => {
+    loginMutation.mutate({
+        username: values.username,
+        password: values.password
+    }, {
+        onSuccess: () => {
+            notyf.success('Inicio de sesión exitoso');
+            router.push('/');
+        },
+        onError: (error) => {
+            console.error('Error al iniciar sesión:', error);
+            notyf.error('Usuario o contraseña incorrectos');
+        }
+    });
 };
-
-const login = async (values) => {
-    try {
-        await loginMutation.mutateAsync({
-            username: values.username,
-            password: values.password
-        });
-
-        notyf.success('Inicio de sesión exitoso');
-        router.push('/');
-    } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-        notyf.error('Usuario o contraseña incorrectos');
-    }
-};
-
-const logout = async () => {
-    try {
-        await logoutMutation.mutateAsync();
-        notyf.success('Sesión cerrada correctamente');
-        router.push('/login');
-    } catch (error) {
-        console.error('Error al cerrar sesión:', error);
-        notyf.error('Error al cerrar sesión');
-    }
-};
-
 </script>
+
 <template>
     <div class="flex flex-col min-h-screen">
         <NavbarTerracota />
         <div class="max-w-7xl mx-auto px-5 w-full flex flex-col flex-grow">
             <div class="container flex justify-center items-center mt-10 hidden md:flex">
-                <div class="w-1/3 bg-white shadow-lg rounded-lg p-6">
+                <div class="w-full sm:w-1/3 bg-white shadow-lg rounded-lg p-6">
                     <h2 class="text-2xl font-semibold text-gray-800 text-center">Iniciar Sesión</h2>
 
-                    <Form @submit="login">
+                    <Form @submit="login" :validation-schema="validationSchema" v-slot="{ errors, meta, values }">
                         <div class="mt-4">
                             <label for="username" class="block text-sm font-medium text-gray-700">Nombre de Usuario</label>
-                            <Field name="username" as="input" id="username" v-model="username" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <ErrorMessage name="username" class="text-red-500 text-sm" />
+                            <Field type="text" name="username" v-slot="{ field, meta }" required>
+                                <input v-bind="field" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+                                <ErrorMessage name="username" class="text-red-500 text-sm" v-if="errors.username && meta.dirty" />
+                            </Field>
                         </div>
 
                         <div class="mt-4 relative">
                             <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
-                            <Field :type="showPassword ? 'text' : 'password'" name="password" as="input" id="password" v-model="password" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <button type="button" @click="togglePasswordVisibility" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 mt-6">
-                                <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-                            </button>
-                            <ErrorMessage name="password" class="text-red-500 text-sm" />
+
+                             <Field name="password" v-slot="{ field, meta }" required>
+                                <input type="password" v-bind="field" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+                                <ErrorMessage name="password" class="text-red-500 text-sm" v-if="errors.password && meta.dirty" />
+                            </Field>
                         </div>
 
                         <div v-if="errorMessage" class="mt-4 text-red-500 text-center">{{ errorMessage }}</div>
 
                         <div class="mt-6">
                             <button type="submit"
-                                class="w-full py-2 px-4 bg-azul-suave text-white hover:bg-azul-suave-dark focus:outline-none focus:ring-2 focus:ring-azul-suave">
-                                Iniciar Sesión
-                            </button>
-                        </div>
-
-                        <div class="mt-4 text-center">
-                            <p class="text-sm text-gray-600">
-                                ¿No tienes cuenta?
-                                <router-link to="/register" class="text-azul-suave hover:underline">Regístrate aquí</router-link>
-                            </p>
-                        </div>
-                    </Form>
-                </div>
-            </div>
-
-            <div class="container flex flex-col items-center mt-10 md:hidden">
-                <div class="w-full max-w-xs bg-white shadow-lg rounded-lg p-6">
-                    <h2 class="text-xl font-semibold text-gray-800 text-center">Iniciar Sesión</h2>
-
-                    <Form @submit="login">
-                        <div class="mt-4">
-                            <label for="username" class="block text-sm font-medium text-gray-700">Correo Electrónico</label>
-                            <Field name="username" as="input" id="username" v-model="username" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <ErrorMessage name="username" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
-                            <Field :type="showPassword ? 'text' : 'password'" name="password" as="input" id="password" v-model="password" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <button type="button" @click="togglePasswordVisibility" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 mt-6">
-                                <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-                            </button>
-                            <ErrorMessage name="password" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div v-if="errorMessage" class="mt-4 text-red-500 text-center">{{ errorMessage }}</div>
-
-                        <div class="mt-6">
-                            <button type="submit"
-                                class="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-azul-suave">
+                                class="w-full py-2 px-4 bg-azul-suave text-white hover:bg-azul-suave-dark focus:outline-none focus:ring-2 focus:ring-azul-suave"
+                                :disabled="!meta.valid">
                                 Iniciar Sesión
                             </button>
                         </div>
@@ -140,7 +84,6 @@ const logout = async () => {
                     </Form>
                 </div>
             </div>
-
         </div>
 
         <Footer />
@@ -151,6 +94,12 @@ const logout = async () => {
 @media (max-width: 900px) {
     .container {
         padding: 1rem;
+    }
+    .w-full {
+        width: 100%;
+    }
+    .sm\:w-1\/3 {
+        width: 100%;
     }
 }
 </style>
--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -6,14 +6,10 @@ import Footer from '../components/Footer.vue';
 import { useCreateCustomer } from '@/data-layer/hooks/customers';
 import { useCreateHotelOwner } from '@/data-layer/hooks/hotelOwners';
 import { Notyf } from 'notyf';
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { Form, Field, ErrorMessage, useForm } from 'vee-validate';
+import * as yup from 'yup';
 
 const notyf = new Notyf();
-const username = ref('');
-const phone = ref('');
-const password = ref('');
-const confirmPassword = ref('');
-const role = ref('customer'); // Default role set to 'customer'
 const router = useRouter();
 const { mutateAsync: createCustomer } = useCreateCustomer();
 const { mutateAsync: createHotelOwner } = useCreateHotelOwner();
@@ -21,153 +17,134 @@ const { mutateAsync: createHotelOwner } = useCreateHotelOwner();
 const showPassword = ref(false);
 const showConfirmPassword = ref(false);
 
-const togglePasswordVisibility = () => {
-    showPassword.value = !showPassword.value;
-};
-
-const toggleConfirmPasswordVisibility = () => {
-    showConfirmPassword.value = !showConfirmPassword.value;
-};
+// Esquema de validación usando Yup
+const validationSchema = yup.object({
+  username: yup.string().required('El nombre de usuario es obligatorio'),
+  email: yup.string().email('El correo electrónico no es válido').required('El correo electrónico es obligatorio'),
+  phone: yup.string().matches(/^\+34\d{9}$/, 'El teléfono debe ser válido').required('El teléfono es obligatorio'),
+  password: yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('La contraseña es obligatoria'),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], 'Las contraseñas no coinciden').required('Es necesario confirmar la contraseña'),
+  role: yup.string().oneOf(['customer', 'hotel_owner'], 'Selecciona un rol válido').required('Es obligatorio seleccionar un rol'),
+});
 
 const register = async (values) => {
-    const currentDate = new Date().toISOString().split('T')[0];
+  const currentDate = new Date().toISOString().split('T')[0];
 
-    try {
-        if (values.role === 'hotel_owner') {
-            await createHotelOwner(
-                {
-                    username: values.username,
-                    email: values.email,
-                    phone: values.phone,
-                    password: values.password,
-                    role: values.role,
-                    date_joined: currentDate,
-                },
-                {
-                    onSuccess: () => {
-                        notyf.success("Registro exitoso");
-                        router.push('/login');
-                    },
-                    onError: (error) => {
-                        console.error('Error al registrarse:', error);
-                        notyf.error("Error en el registro");
-                    },
-                }
-            );
-        } else {
-            await createCustomer(
-                {
-                    username: values.username,
-                    email: values.email,
-                    phone: values.phone,
-                    password: values.password,
-                    role: values.role,
-                    date_joined: currentDate,
-                },
-                {
-                    onSuccess: () => {
-                        notyf.success("Registro exitoso");
-                        router.push('/login');
-                    },
-                    onError: (error) => {
-                        console.error('Error al registrarse:', error);
-                        notyf.error("Error en el registro");
-                    },
-                }
-            );
+  try {
+    if (values.role === 'hotel_owner') {
+      await createHotelOwner(
+        {
+          username: values.username,
+          email: values.email,
+          phone: values.phone,
+          password: values.password,
+          role: values.role,
+          date_joined: currentDate,
+        },
+        {
+          onSuccess: () => {
+            notyf.success("Registro exitoso");
+            router.push('/login');
+          },
+          onError: (error) => {
+            console.error('Error al registrarse:', error);
+            notyf.error("Error en el registro");
+          },
         }
-    } catch (error) {
-        notyf.error('Hubo un error al registrarse. Inténtalo de nuevo.');
+      );
+    } else {
+      await createCustomer(
+        {
+          username: values.username,
+          email: values.email,
+          phone: values.phone,
+          password: values.password,
+          role: values.role,
+          date_joined: currentDate,
+        },
+        {
+          onSuccess: () => {
+            notyf.success("Registro exitoso");
+            router.push('/login');
+          },
+          onError: (error) => {
+            console.error('Error al registrarse:', error);
+            notyf.error("Error en el registro");
+          },
+        }
+      );
     }
+  } catch (error) {
+    notyf.error('Hubo un error al registrarse. Inténtalo de nuevo.');
+  }
 };
 </script>
 
 <template>
-    <div class="flex flex-col min-h-screen">
-        <NavbarTerracota />
-        <div class="max-w-7xl mx-auto px-5 w-full flex flex-col flex-grow">
-            <div class="container flex justify-center items-center mt-10 hidden md:flex">
-                <div class="w-1/3 bg-white shadow-lg rounded-lg p-6">
-                    <h2 class="text-2xl font-semibold text-gray-800 text-center">Registrarse</h2>
+  <div class="flex flex-col min-h-screen">
+    <NavbarTerracota />
+    <div class="max-w-7xl mx-auto px-5 w-full flex flex-col flex-grow">
+      <div class="container flex justify-center items-center mt-10">
+        <div class="w-full sm:w-1/3 bg-white shadow-lg rounded-lg p-6">
+          <h2 class="text-2xl font-semibold text-gray-800 text-center">Registrarse</h2>
 
-                    <Form @submit="register">
-                        <div class="mt-4 relative">
-                            <label for="username" class="block text-sm font-medium text-gray-700">Nombre de Usuario</label>
-                            <Field name="username" as="input" id="username" v-model="username" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <ErrorMessage name="username" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="email" class="block text-sm font-medium text-gray-700">Correo Electrónico</label>
-                            <Field name="email" as="input" id="email" v-model="email" rules="required|email"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <ErrorMessage name="email" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="phone" class="block text-sm font-medium text-gray-700">Teléfono</label>
-                            <input v-validate="{ required: true, regex: /^\+34\d{9}$/ }"
-                                   name="phone"
-                                   v-model="phone"
-                                   type="text"
-                                   class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                   required />
-                            <ErrorMessage name="phone" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="role" class="block text-sm font-medium text-gray-700">¿Qué eres?</label>
-                            <Field name="role" as="select" id="role" v-model="role" rules="required"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required>
-                                <option value="customer">Cliente</option>
-                                <option value="hotel_owner">Dueño de hotel</option>
-                            </Field>
-                            <ErrorMessage name="role" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
-                            <Field :type="showPassword ? 'text' : 'password'" name="password" as="input" id="password" v-model="password" rules="required|min:6"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <button type="button" @click="togglePasswordVisibility" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 mt-6">
-                                <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-                            </button>
-                            <ErrorMessage name="password" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-4 relative">
-                            <label for="confirmPassword" class="block text-sm font-medium text-gray-700">Confirmar contraseña</label>
-                            <Field :type="showConfirmPassword ? 'text' : 'password'" name="confirmPassword" as="input" id="confirmPassword" v-model="confirmPassword" rules="required|confirmed:password"
-                                class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500"
-                                required />
-                            <button type="button" @click="toggleConfirmPasswordVisibility" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 mt-6">
-                                <i :class="showConfirmPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-                            </button>
-                            <ErrorMessage name="confirmPassword" class="text-red-500 text-sm" />
-                        </div>
-
-                        <div class="mt-6">
-                            <button type="submit"
-                                class="w-full py-2 px-4 bg-azul-suave text-white hover:bg-azul-suave-dark focus:outline-none focus:ring-2 focus:ring-azul-suave">
-                                Registrarse
-                            </button>
-                        </div>
-
-                        <div class="mt-4 text-center">
-                            <p class="text-sm text-gray-600">
-                                ¿Ya tienes cuenta?
-                                <router-link to="/login" class="text-azul-suave hover:underline">Inicia sesión aquí</router-link>
-                            </p>
-                        </div>
-                    </Form>
-                </div>
+          <!-- Formulario con validación -->
+          <Form :validation-schema="validationSchema" @submit="register">
+            <div class="mt-4 relative">
+              <label for="username" class="block text-sm font-medium text-gray-700">Nombre de Usuario</label>
+              <Field name="username" as="input" id="username" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+              <ErrorMessage name="username" class="text-red-500 text-sm" />
             </div>
+
+            <div class="mt-4 relative">
+              <label for="email" class="block text-sm font-medium text-gray-700">Correo Electrónico</label>
+              <Field name="email" as="input" id="email" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+              <ErrorMessage name="email" class="text-red-500 text-sm" />
+            </div>
+
+            <div class="mt-4 relative">
+              <label for="phone" class="block text-sm font-medium text-gray-700">Teléfono</label>
+              <Field name="phone" as="input" id="phone" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+              <ErrorMessage name="phone" class="text-red-500 text-sm" />
+            </div>
+
+            <div class="mt-4 relative">
+              <label for="role" class="block text-sm font-medium text-gray-700">¿Qué eres?</label>
+              <Field name="role" as="select" id="role" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500">
+                <option value="customer">Cliente</option>
+                <option value="hotel_owner">Dueño de hotel</option>
+              </Field>
+              <ErrorMessage name="role" class="text-red-500 text-sm" />
+            </div>
+
+            <div class="mt-4 relative">
+              <label for="password" class="block text-sm font-medium text-gray-700">Contraseña</label>
+              <Field :type="showPassword ? 'text' : 'password'" name="password" as="input" id="password" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+              <ErrorMessage name="password" class="text-red-500 text-sm" />
+            </div>
+
+            <div class="mt-4 relative">
+              <label for="confirmPassword" class="block text-sm font-medium text-gray-700">Confirmar contraseña</label>
+              <Field :type="showConfirmPassword ? 'text' : 'password'" name="confirmPassword" as="input" id="confirmPassword" class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-azul-suave focus:border-blue-500" />
+              <ErrorMessage name="confirmPassword" class="text-red-500 text-sm" />
+            </div>
+
+            <div class="mt-6">
+              <button type="submit" class="w-full py-2 px-4 bg-azul-suave text-white hover:bg-azul-suave-dark focus:outline-none focus:ring-2 focus:ring-azul-suave">
+                Registrarse
+              </button>
+            </div>
+
+            <div class="mt-4 text-center">
+              <p class="text-sm text-gray-600">
+                ¿Ya tienes cuenta?
+                <router-link to="/login" class="text-azul-suave hover:underline">Inicia sesión aquí</router-link>
+              </p>
+            </div>
+          </Form>
         </div>
-        <Footer />
+      </div>
     </div>
+    <Footer />
+  </div>
 </template>
