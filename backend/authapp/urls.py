c2e16b7631e1feeafeab4dc883410afceb0cd782
David González Martínez <me@davidgonmar.me>
2025-03-21 11:37:45 +0100

    Revert "Reestablecimiento de contraseña"

--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -1,17 +1,8 @@
-from authapp.views import (LoginView, PasswordResetConfirmView,
-                           PasswordResetView, RegisterView, UserInfoView)
+from authapp.views import LoginView, RegisterView, UserInfoView
 from django.urls import path
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("user-info/", UserInfoView.as_view(), name="view user info"),
     path("register/", RegisterView.as_view(), name="register"),
-    path(
-        "email-password-reset/", PasswordResetView.as_view(), name="email new password"
-    ),
-    path(
-        "password-reset-confirm/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(),
-        name="password reset",
-    ),
 ]
