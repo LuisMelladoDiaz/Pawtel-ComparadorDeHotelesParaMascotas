49046d0976dbf356940916c8f1450c00d1114308
KSTY <mancasvel@icloud.com>
2025-03-23 23:16:19 +0100

    feat(queries): hotel images queries #205

---
+++ frontend/src/data-layer/api/hotelImages.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export type HotelImage = {
+  id?: number;
+  image: string;
+  is_cover: boolean;
+  hotel: number;
+};
+
+export const fetchHotelImage = async (hotelId: number, imageId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/`;
+  const response = await axios.get(url);
+  return response.data as HotelImage;
+};
+
+export const fetchAllHotelImages = async (hotelId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/all/`;
+  const response = await axios.get(url);
+  return response.data as HotelImage[];
+};
+
+export const uploadHotelImage = async (hotelId: number, image: string, isCover: boolean) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/upload/`;
+  const response = await axios.post(url, { image, is_cover: isCover });
+  return response.data as HotelImage;
+};
+
+export const updateHotelImage = async (hotelId: number, imageId: number, image: string, isCover: boolean) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/update/`;
+  const response = await axios.put(url, { image, is_cover: isCover });
+  return response.data as HotelImage;
+};
+
+export const partialUpdateHotelImage = async (hotelId: number, imageId: number, image: string, isCover: boolean) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/patch/`;
+  const response = await axios.patch(url, { image, is_cover: isCover });
+  return response.data as HotelImage;
+};
+
+export const deleteHotelImage = async (hotelId: number, imageId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/delete/`;
+  const response = await axios.delete(url);
+  return response.data;
+};
+
+export const setCoverImage = async (hotelId: number, imageId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/set-cover/`;
+  const response = await axios.put(url);
+  return response.data as HotelImage;
+};
+
+export const unsetCoverImage = async (hotelId: number, imageId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/${imageId}/set-non-cover/`;
+  const response = await axios.put(url);
+  return response.data as HotelImage;
+};
+
+export const fetchCoverImage = async (hotelId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/cover/`;
+  const response = await axios.get(url);
+  return response.data as HotelImage;
+};
+
+export const fetchNonCoverImages = async (hotelId: number) => {
+  const url = `${API_BASE_URL}/hotels/${hotelId}/hotel-images/non-cover/`;
+  const response = await axios.get(url);
+  return response.data as HotelImage[];
+};
---
+++ frontend/src/data-layer/hooks/hotelImages.ts
@@ -0,0 +1,89 @@
+import { useQuery, useMutation } from '@tanstack/vue-query';
+import { ref } from 'vue';
+import {
+  fetchHotelImage,
+  fetchAllHotelImages,
+  uploadHotelImage,
+  updateHotelImage,
+  partialUpdateHotelImage,
+  deleteHotelImage,
+  setCoverImage,
+  unsetCoverImage,
+  fetchCoverImage,
+  fetchNonCoverImages,
+} from '@/data-layer/api/hotelImages';
+
+export const useGetHotelImage = (hotelId: number, imageId: number) => {
+  return useQuery({
+    queryKey: ['hotel-image', hotelId, imageId],
+    queryFn: () => fetchHotelImage(hotelId, imageId),
+    staleTime: 1000 * 60,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useGetAllHotelImages = (hotelId: number) => {
+  return useQuery({
+    queryKey: ['hotel-images', hotelId],
+    queryFn: () => fetchAllHotelImages(hotelId),
+    staleTime: 1000 * 60,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useUploadHotelImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; image: string; isCover: boolean }) =>
+      uploadHotelImage(data.hotelId, data.image, data.isCover),
+  });
+};
+
+export const useUpdateHotelImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; imageId: number; image: string; isCover: boolean }) =>
+      updateHotelImage(data.hotelId, data.imageId, data.image, data.isCover),
+  });
+};
+
+export const usePartialUpdateHotelImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; imageId: number; image: string; isCover: boolean }) =>
+      partialUpdateHotelImage(data.hotelId, data.imageId, data.image, data.isCover),
+  });
+};
+
+export const useDeleteHotelImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; imageId: number }) => deleteHotelImage(data.hotelId, data.imageId),
+  });
+};
+
+export const useSetCoverImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; imageId: number }) => setCoverImage(data.hotelId, data.imageId),
+  });
+};
+
+export const useUnsetCoverImage = () => {
+  return useMutation({
+    mutationFn: (data: { hotelId: number; imageId: number }) => unsetCoverImage(data.hotelId, data.imageId),
+  });
+};
+
+export const useGetCoverImage = (hotelId: number) => {
+  return useQuery({
+    queryKey: ['hotel-cover-image', hotelId],
+    queryFn: () => fetchCoverImage(hotelId),
+    staleTime: 1000 * 60,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const useGetNonCoverImages = (hotelId: number) => {
+  return useQuery({
+    queryKey: ['hotel-non-cover-images', hotelId],
+    queryFn: () => fetchNonCoverImages(hotelId),
+    staleTime: 1000 * 60,
+    refetchOnWindowFocus: false,
+  });
+};
