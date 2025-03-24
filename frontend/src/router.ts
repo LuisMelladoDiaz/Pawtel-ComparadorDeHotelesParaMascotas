c4c46a0d3a4a75e45bccb29a0807833045eae710
David González Martínez <me@davidgonmar.me>
2025-03-24 17:28:21 +0100

    feat: add automatic route guarding. #210

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HotelDetailsView from './views/HotelDetailView.vue';
 import TemplateScreen1 from './views/TemplateScreen1.vue'
-import TemplateScreen2 from './views/TemplateScreen2.vue'
 import UserProfile from './views/UserProfile.vue'
 import HotelListScreen from './views/HotelListScreen.vue'
 import LoginView from './views/LoginView.vue'
@@ -14,6 +13,8 @@ import Contact from './views/Contact.vue';
 import { h, type Component} from 'vue';
 import LayoutDefault from './views/LayoutDefault.vue';
 import LayoutWithFilter from './views/LayoutWithFilter.vue';
+import { type RouteRecordRaw } from 'vue-router';
+import { useRoleQuery } from './data-layer/auth';
 
 type ComponentLike = Component | (() => Promise<Component>);
 
@@ -32,81 +33,141 @@ export function createComponent({ layout, component }: CreateComponentOptions): 
   };
 }
 
+enum AuthRequirement {
+  LOGGED_IN_CUSTOMER = 'logged_in_customer',
+  LOGGED_IN_HOTEL_OWNER = 'logged_in_hotel_owner',
+  LOGGED_OUT = 'logged_out',
+}
+
+const ALLOW_ALL = [
+  AuthRequirement.LOGGED_IN_CUSTOMER,
+  AuthRequirement.LOGGED_IN_HOTEL_OWNER,
+  AuthRequirement.LOGGED_OUT,
+];
+
+const ALLOW_LOGGED_IN = [
+  AuthRequirement.LOGGED_IN_CUSTOMER,
+  AuthRequirement.LOGGED_IN_HOTEL_OWNER,
+];
+
+function transformRoutes(routes: RouteRecordRaw[]): RouteRecordRaw[] {
+  return routes.map((route) => {
+    const newRoute: RouteRecordRaw = { ...route };
+    if (newRoute.meta && (newRoute.meta as any).allowedAuthStates) {
+      if ((newRoute.meta as any).hasOwnProperty('allowedAuthStates')) {
+        if (!Array.isArray((newRoute.meta as any).allowedAuthStates)) {
+          throw new Error('allowedAuthStates must be an array, got: ' + (newRoute.meta as any).allowedAuthStates);
+        }
+      }
+      const allowedStates = (newRoute.meta as any).allowedAuthStates as AuthRequirement[];
+      newRoute.beforeEnter = async (to, from) => {
+        console.log('beforeEnter', to, from);
+        const q = useRoleQuery();
+        await q.suspense();
+        if (q.isLoading.value) {
+          throw new Error('Loading role');
+        }
+        // if error, assume logged out, redirect to login
+        if (q.error.value) {
+          return '/login';
+        }
+        const role = q.data.value;
+        const state = role == 'customer' ? AuthRequirement.LOGGED_IN_CUSTOMER : role == 'hotel_owner' ? AuthRequirement.LOGGED_IN_HOTEL_OWNER : AuthRequirement.LOGGED_OUT;
+        if (!allowedStates.includes(state)) {
+           return '/';
+        }
+        return true;
+      };
+    }
+
+    // 3) Recursively handle any children
+    if (route.children && route.children.length > 0) {
+      newRoute.children = transformRoutes(route.children);
+    }
+
+    return newRoute;
+  });
+}
+
 
 const routes = [
   {
     path: '/',
-    component: Home
+    component: Home,
+    meta: {
+      allowedAuthStates: ALLOW_ALL,
+    },
   },
   {
     path: '/sobre-nosotros',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: AboutUs,
-    })
+    component: createComponent({ layout: LayoutDefault, component: AboutUs }),
+    meta: {
+      allowedAuthStates: ALLOW_ALL,
+    }
   },
   {
     path: '/contacto',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: Contact,
-    })
+    component: createComponent({ layout: LayoutDefault, component: Contact }),
+    meta: {
+      allowedAuthStates: ALLOW_ALL,
+    },
   },
   {
     path: '/hotels',
-    component: createComponent({
-      layout: LayoutWithFilter,
-      component: HotelListScreen,
-    })
+    component: createComponent({ layout: LayoutWithFilter, component: HotelListScreen }),
+    meta: {
+      allowedAuthStates: ALLOW_ALL,
+    },
   },
   {
     path: '/hotel/:id',
     name: 'HotelDetail',
-    component: createComponent({
-      layout: LayoutWithFilter,
-      component: HotelDetailsView,
-    })
+    component: createComponent({ layout: LayoutWithFilter, component: HotelDetailsView }),
+    meta: {
+      allowedAuthStates: ALLOW_ALL,
+    },
   },
   {
     path: '/login',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: LoginView,
-    })
+    component: createComponent({ layout: LayoutDefault, component: LoginView }),
+    meta: {
+      allowedAuthStates: [AuthRequirement.LOGGED_OUT],
+    },
   },
   {
     path: '/register',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: RegisterView,
-    })
+    component: createComponent({ layout: LayoutDefault, component: RegisterView }),
+    meta: {
+      allowedAuthStates: [AuthRequirement.LOGGED_OUT],
+    },
   },
   {
     path: '/user-profile',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: UserProfile,
-    })
-  },
-  {
-    path: '/mis-hoteles',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: HotelOwnerPanel,
-    })
+    component: createComponent({ layout: LayoutDefault, component: UserProfile }),
+    meta: {
+      allowedAuthStates: ALLOW_LOGGED_IN, // TODO
+    },
   },
   {
     path: '/mis-reservas',
-    component: createComponent({
-      layout: LayoutDefault,
-      component: TemplateScreen1,
-    })
+    component: createComponent({ layout: LayoutDefault, component: TemplateScreen1 }),
+    meta: {
+      allowedAuthStates: [AuthRequirement.LOGGED_IN_CUSTOMER],
+    },
+  },
+  {
+    path: '/mis-hoteles',
+    component: createComponent({ layout: LayoutDefault, component: HotelOwnerPanel }),
+    meta: {
+      allowedAuthStates: [AuthRequirement.LOGGED_IN_HOTEL_OWNER],
+    },
   },
 ];
 
+
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: transformRoutes(routes),
 });
 
 export default router;
