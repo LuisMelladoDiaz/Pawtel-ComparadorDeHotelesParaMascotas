a2f7c7ae6c2d123b2882a53aef1f5a44308234f8
KSTY <mancasvel@icloud.com>
2025-03-25 20:53:12 +0100

    fix(screen): router use #225

--- frontend/src/views/HotelDetailView.vue
+++ frontend/src/views/HotelDetailView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 import HotelDetailCard from '../components/HotelDetailCard.vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import { useGetHotelById } from '@/data-layer/hooks/hotels';
@@ -9,11 +10,8 @@ import detalles3 from '../assets/hoteles/detalles3.jpg';
 import detalles4 from '../assets/hoteles/detalles4.jpg';
 import hotelpic from '../assets/hoteles/hotel1.jpg';
 
-
-const hotelId = computed(() => {
-  const pathParts = window.location.pathname.split('/');
-  return Number(pathParts[pathParts.length - 1]) || null;
-});
+const route = useRoute();
+const hotelId = computed(() => Number(route.params.id));
 
 const { data: apiHotel, isLoading, error } = useGetHotelById(hotelId);
 
@@ -47,24 +45,27 @@ const hotel = computed(() => ({
     <div class="hidden md:flex items-center max-w-7xl mx-auto px-5 w-full flex-col flex-grow">
       <div class="bg-white shadow-md py-3 flex justify-between max-w-7xl mx-auto w-full px-10 text-black text-lg border-b">
         <div class="flex flex-row w-full justify-center space-x-2">
-          <a
-            :href="`/hotel/${hotelId}`"
+          <router-link
+            :to="$route.path"
             class="w-1/2 text-center py-2 px-4 bg-gray-200 rounded-tl-md rounded-bl-md cursor-default"
           >
             Vista General
-          </a>
+          </router-link>
 
-          <a
-            :href="`/hotel/${hotelId}/rooms`"
+          <router-link
+            :to="`${$route.path}/rooms`"
             class="w-1/2 text-center py-2 px-4 bg-gray-100 rounded-tr-md rounded-br-md hover:bg-azul-suave hover:text-white transition duration-200 ease-in-out"
+            :class="{ 'bg-blue-500 text-white': $route.path === `/hotel/${$route.params.id}/rooms` }"
           >
             Habitaciones y Precios
-          </a>
+          </router-link>
         </div>
       </div>
 
       <div class="max-w-7xl mx-auto py-10 w-full">
-        <HotelDetailCard v-bind="hotel" />
+        <HotelDetailCard
+          v-bind="hotel"
+        />
       </div>
     </div>
 
@@ -72,19 +73,20 @@ const hotel = computed(() => ({
     <div class="md:hidden flex flex-col items-center px-4 py-6">
       <div class="bg-white shadow-md py-3 flex items-center justify-between max-w-7xl mx-auto w-full px-10 text-black text-lg border-b">
         <div class="flex flex-col items-center w-full justify-center space-y-2">
-          <a
-            :href="`/hotel/${hotelId}`"
+          <router-link
+            :to="$route.path"
             class="w-full text-center py-2 px-4 bg-gray-200 rounded-md cursor-default"
           >
             Vista General
-          </a>
+          </router-link>
 
-          <a
-            :href="`/hotel/${hotelId}/rooms`"
+          <router-link
+            :to="`${$route.path}/rooms`"
             class="w-full text-center py-2 px-4 bg-gray-100 rounded-md hover:bg-azul-suave hover:text-white transition duration-200 ease-in-out"
+            :class="{ 'bg-blue-500 text-white': $route.path === `/hotel/${$route.params.id}/rooms` }"
           >
             Habitaciones y Precios
-          </a>
+          </router-link>
         </div>
       </div>
 
